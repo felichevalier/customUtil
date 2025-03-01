fc31ddf8a0afcf295b0f1b96aba2f7265390f1ff
grand_father <grandfather6007@gmail.com>
2025-03-01 17:38:34 +0900

    ライブラリの追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,9 +7,16 @@ version = "1.0.0"
 
 repositories {
     mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/") {
+        name = "papermc-repo"
+    }
+    maven("https://oss.sonatype.org/content/groups/public/") {
+        name = "sonatype"
+    }
 }
 
 dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21.1-R0.1-SNAPSHOT")
     testImplementation(kotlin("test"))
 }
 
